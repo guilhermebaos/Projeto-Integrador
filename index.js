1e9f7d0ed9ec9387cd8bcaf8161c821eba2d5ba0
Guilherme Botelho <guilhermebaos@gmail.com>
2024-05-16 16:49:23 +0100

    Barra de progresso

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const audioContext = new AudioContext()
 
 const testing = document.getElementById("testing")
 
+// Search window when looking at know string
+const tol = 40
+
 // Start Processing the Audio
-const tunerKnownString = async (context, meterElement) => {
+const tunerKnownString = async (context) => {
     await context.audioWorklet.addModule("tuner-known-string.js")
 
     // Setup Audio Recording
@@ -28,22 +31,70 @@ const tunerKnownString = async (context, meterElement) => {
 }
 
 
+
+function updateBar(progressBar, target, current) {
+    // Percentagem de distância do alvo, tendo em conta a tolerância
+    pos = (current - target) / (2 * tol) + 0.5
+
+    if (pos < 0) {
+        pos = 0
+    } else if (pos > 1) {
+        pos = 1
+    }
+
+    gsap.to(progressBar, {
+        x: `${target}%`,
+        duration: 2,
+    });
+}
+
+
 // Start Tuning
 let tunerActive = false
 window.addEventListener("load", async () => {
     // Get HTML Elements
     const buttonStart = document.getElementById("button-start")
     const buttonStop = document.getElementById("button-stop")
-    const meterEl = document.getElementById("volume-meter")
+
+    const progressBarContainer = document.querySelector('.progress-bar__container')
+    const progressBar = document.querySelector('.progress-bar')
+
+    const progressBarStates = [0, 7, 27, 34, 68, 80, 95, 100];
+
+    let time = 0;
+    let endState = 100;
+
+    progressBarStates.forEach(state => {
+    let randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+        if(state == endState){
+        gsap.to(progressBar, {
+            x: `${state}%`,
+            duration: 2,
+            backgroundColor: '#4895ef',
+            onComplete: () => {
+            progressBarText.style.display = "initial";
+            progressBarContainer.style.boxShadow = '0 0 5px #4895ef';
+            }
+        });
+        }else{
+        gsap.to(progressBar, {
+            x: `${state}%`,
+            duration: 2,
+        });
+        }
+    }, randomTime + time);
+    time += randomTime;
+    })
+
 
     buttonStart.disabled = false
     buttonStop.disabled = true
-    meterEl.disabled = false
 
     // Resume the AudioContext and start recording 
     buttonStart.addEventListener("click", async () => {
         if (!tunerActive) {
-            await tunerKnownString(audioContext, meterEl)
+            await tunerKnownString(audioContext)
             tunerActive = true
         }
         
